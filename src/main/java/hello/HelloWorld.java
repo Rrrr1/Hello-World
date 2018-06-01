48182a178895d640fc051fccd1f8d69f1d43f8bf
Erwan Mary <Erwan@MacBook-Air-de-Erwan.local>
2018-06-01 16:15:33 +0200

    dec

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,7 @@
 package hello;
 
 import org.joda.time.LocalTime;
-//import org.apache.commons.math3.analysis.function.Log;
+import org.apache.commons.math3.analysis.function.Log;
 //import org.apache.commons.math3.analysis.function.Abs;
 
 public class HelloWorld {
@@ -11,8 +11,8 @@ public class HelloWorld {
 		System.out.println("The current local time is: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
-                //Log logarithme = new Log();
-                //System.out.println(logarithme.value(54));
+                Log logarithme = new Log();
+                System.out.println(logarithme.value(54));
                 //Abs absolue = new Abs();
                 //System.out.println(absolue.value(-5.978));
 	}
